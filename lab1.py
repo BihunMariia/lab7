cb0e1f2cc62f005bba55325139f5a1d58b55d57e
BihunMariia <mariabigun27@gmail.com>
2023-05-15 16:13:58 +0300

    adding my lab with changes

--- lab1.py
+++ lab1.py
@@ -1,35 +1,36 @@
 class BoardGame:
-    def __init__(self, title="", minPlayers=0, maxPlayers=0):
+    def __init__(self, title="", min_players=0, max_players=0):
         self.title = title
-        self.minPlayers = minPlayers
-        self.maxPlayers = maxPlayers
-        self.currentPlayers = 0
+        self.min_players = min_players
+        self.max_players = max_players
+        self.current_players = 0
 
     def __str__(self):
-        return f"MyClass instance, title: {self.title}, minPlayers: {self.minPlayers}, maxPlayers: {self.maxPlayers}"
+        return f"MyClass instance, title: {self.title}, minPlayers: {self.min_players}, maxPlayers: {self.max_players}"
     
     def add_player(self):
-        if self.currentPlayers < self.maxPlayers:
-            self.currentPlayers += 1
+        if self.current_players < self.max_players:
+            self.current_players += 1
             print("Гравець доданий.")
         else:
             print("Досягнуто максимальну кількість гравців.")
 
     def remove_player(self):
-        if self.currentPlayers > 0:
-            self.currentPlayers -= 1
+        if self.current_players > 0:
+            self.current_players -= 1
             print("Гравець видалений.")
         else:
             print("Немає гравців у грі.")
 
     def can_play(self):
-        return self.currentPlayers >= self.minPlayers
+        return self.current_players >= self.min_players
     
-    defaultBoardGame = None
+    default_board_game = None
 
     @staticmethod
-    def getInstance():
-        if BoardGame.defaultBoardGame is None:
-            BoardGame.defaultBoardGame = BoardGame()
-        return BoardGame.defaultBoardGame
+    def get_instance():
+        if BoardGame.default_board_game is None:
+            BoardGame.default_board_game = BoardGame()
+        return BoardGame.default_board_game
+
 
--- main.py
+++ main.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     game3 = BoardGame("Chess", 2, 2)
     game4 = BoardGame("Monopoly", 2, 4)
     
-    game5 = BoardGame.getInstance()
-    game6 = BoardGame.getInstance()
+    game5 = BoardGame.get_instance()
+    game6 = BoardGame.get_instance()
     
     games.extend([game1, game2, game3, game4, game5, game6])
     
-    for game in games:
-        print(id(game))
+    for id, value in enumerate(games):
+        print(id, value)
